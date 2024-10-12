052e9d9f3bc4d9c2df5e54b3b318356dda15cf6f
MindMiner25 <spatel084.btech2021@min.nitrr.ac.in>
2024-10-13 01:28:43 +0530

    Create understanding_functions.js

---
+++ Functions/understanding_functions.js
@@ -0,0 +1,40 @@
+// PROBLEM STATEMENT 
+// Find the multiplication of two numbers a and b without using *-operator
+// Find the result applying all possible functions in JS.
+
+// INTUITION :-
+// a = e^ln(a)
+// b = e^ln(b)
+// product = a*b = e^(ln(a)+ln(b)).
+
+// APPROACH
+// 1. Find the natural log of a and b.
+// 2. Take sum
+// 3. Evaluate anti-log of sum.
+
+// BASIC SYNTAX
+
+function sum(a, b) {
+    const sum = a + b;
+    return sum;      // If noting is returned then the function returns undefined.
+}
+
+console.log("Basic Function : ",sum(5,6));
+
+// ANONYMOUS FUNCTION
+
+var sum = function (a, b) {
+    const sum = a + b;
+    return sum;      
+}
+
+console.log("Anonymos Function : ",sum(7, 11));
+
+// IIFE --> Immediately Invoked Function Expression
+
+var sum = (function(a, b){
+    const sum = a + b;
+    return sum;
+})(3, 5)
+
+console.log("IIFE : ", sum)
