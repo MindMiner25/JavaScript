895996bcb5f40c7b6001d77b278310e8387a9a0e
MindMiner25 <spatel084.btech2021@min.nitrr.ac.in>
2024-12-27 14:06:33 +0530

    scheduling

--- Advanced_Working_with_functions/scheduling.js
+++ Advanced_Working_with_functions/scheduling.js
@@ -1,14 +1,56 @@
-// Function call-scheduling.
+// Scheduling
 
-function sum(a,b) {
-    alert(a+b);
+// setTimeout(func|code,delay,[arg1,arg2,arg3,...]) 
+// Allows to run a function once after an interval of time.
+
+let sayHi = (phrase,name) => {
+    alert(phrase+','+name);
 }
 
-setTimeout(sum,2000,8,9);
+setTimeout(sayHi,2000,"Morning","Sudhanshu");
 
-// clearTimeout() -> to call of a scheduling
+// If the argument func|code => string, JS creates a function from it.
 
-let timerId = setInterval(()=>alert("tick"),3000);
+setTimeout("alert('Good-Morning Sudhanshu')",3000);
 
-setTimeout(()=>{clearInterval(timerId), alert("stop")},10000);
+// So we can define a function inside a setTimeout().
 
+let timerId = setTimeout((phrase,name)=>alert(phrase+","+name),3000,"Good-Morning","Sudhanshu");
+
+// canceling the scheduling of a function.
+
+clearTimeout(timerId);
+
+// setInterval(func|code,delay,[arg1,arg2,arg3,...])
+// Allows to run a function regularly after an interval of time.
+
+// Nested setTimeout
+
+// PROBLEM
+// we need to write a service that sends a request to the server every 5 seconds asking for data, 
+// but in case the server is overloaded, 
+// it should increase the interval to 10, 20, 40 seconds…
+
+// Here we require a nested setTimeout.
+
+// Write a function printNumbers(from, to) that outputs a number every second, starting from from and ending with to.
+
+// Make two variants of the solution.
+
+// Using setInterval.
+// Using nested setTimeout.
+
+let printNumbers = (from,to) => {
+    // setInterval(()=>{
+    //     if (from <= to) alert(from++);
+    // },1000);
+
+    let number = setTimeout(function f() {
+        if (from <= to) alert(from++);
+        number = setTimeout(f,1000);
+    },1000) 
+
+    // Delay has been set precisely.
+};
+
+printNumbers(1,10);
